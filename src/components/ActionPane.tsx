cdf93a455d76cf277e1520e1259ee3c381bf8d02
Harry Thoma <thoma20@up.edu>
2022-09-15 12:55:59 -0700

    Disable buttons when pressed so they can't be double-clicked

--- src/components/ActionPane.tsx
+++ src/components/ActionPane.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
+import 'styles/Game.css';
 import { getDieCountChoices } from "util/actions";
 import { Action } from "model/actionModel";
 import { Game, GameId, Play, Player, UserId } from "model/gameModel";
@@ -15,19 +16,19 @@ export function ActionPane({game, player, dispatchAction}: {game: Game | null, p
     const actions = game.actions[player];
 
     if (actions.includes('RSP')) {
-        return <RspPane dispatchAction={dispatchAction} />;
+        return <RspPane dispatchAction={dispatchAction} game={game} />;
     }
     if (actions.includes('KICKOFF_ELECTION')) {
-        return <KickoffElectionPane dispatchAction={dispatchAction} />
+        return <KickoffElectionPane dispatchAction={dispatchAction} game={game} />
     }
     if (actions.includes('KICKOFF_CHOICE')) {
-        return <KickoffChoicePane dispatchAction={dispatchAction} />;
+        return <KickoffChoicePane dispatchAction={dispatchAction} game={game} />;
     }
     if (actions.includes('TOUCHBACK_CHOICE')) {
-        return <TouchbackChoicePane dispatchAction={dispatchAction} />;
+        return <TouchbackChoicePane dispatchAction={dispatchAction} game={game} />;
     }
     if (actions.includes('ROLL_AGAIN_CHOICE')) {
-        return <RollAgainChoicePane dispatchAction={dispatchAction} />;
+        return <RollAgainChoicePane dispatchAction={dispatchAction} game={game} />;
     }
     if (actions.includes('ROLL')) {
         return <RollPane dispatchAction={dispatchAction} game={game} />;
@@ -45,115 +46,128 @@ export function ActionPane({game, player, dispatchAction}: {game: Game | null, p
     return null;
 }
 
-function RspPane({dispatchAction}: {dispatchAction: ActionDispatch}) {
+type ActionPaneProps = {dispatchAction: ActionDispatch, game: Game};
 
+function RspPane({dispatchAction, game}: ActionPaneProps) {
     const dispatch = getChoiceActionDispatch(dispatchAction, 'RSP');
 
-    return (
-        <div>
-            <ActionButton onClick={() => dispatch('ROCK')}>ROCK</ActionButton>
-            <ActionButton onClick={() => dispatch('PAPER')}>PAPER</ActionButton>
-            <ActionButton onClick={() => dispatch('SCISSORS')}>SCISSORS</ActionButton>
-        </div>
-    );
+    return <ActionButtonGroup
+        onClick={dispatch}
+        gameVersion={game.version}
+        buttons={[
+            {key: 'ROCK', children: 'Rock'},
+            {key: 'PAPER', children: 'Paper'},
+            {key: 'SCISSORS', children: 'Scissors'}
+        ]}
+    />;
 }
 
-function KickoffElectionPane({dispatchAction}: {dispatchAction: ActionDispatch}) {
+function KickoffElectionPane({dispatchAction, game}: ActionPaneProps) {
     const dispatch = getChoiceActionDispatch(dispatchAction, 'KICKOFF_ELECTION');
 
-    return (
-        <div>
-            <ActionButton onClick={() => dispatch('KICK')}>KICK</ActionButton>
-            <ActionButton onClick={() => dispatch('RECIEVE')}>RECIEVE</ActionButton>
-        </div>
-    );
+    return <ActionButtonGroup
+        onClick={dispatch}
+        gameVersion={game.version}
+        buttons={[
+            {key: 'KICK', children: 'Kick'},
+            {key: 'RECIEVE', children: 'Recieve'}
+        ]}
+    />;
 }
 
-function KickoffChoicePane({dispatchAction}: {dispatchAction: ActionDispatch}) {
+function KickoffChoicePane({dispatchAction, game}: ActionPaneProps) {
     const dispatch = getChoiceActionDispatch(dispatchAction, 'KICKOFF_CHOICE');
 
-    return (
-        <div>
-            <ActionButton onClick={() => dispatch('REGULAR')}>REGULAR</ActionButton>
-            <ActionButton onClick={() => dispatch('ONSIDE')}>ONSIDE</ActionButton>
-        </div>
-    );
+    return <ActionButtonGroup
+        onClick={dispatch}
+        gameVersion={game.version}
+        buttons={[
+            {key: 'REGULAR', children: 'Regular'},
+            {key: 'ONSIDE', children: 'Onside'}
+        ]}
+    />;
 }
 
-function TouchbackChoicePane({dispatchAction}: {dispatchAction: ActionDispatch}) {
+function TouchbackChoicePane({dispatchAction, game}: ActionPaneProps) {
     const dispatch = getChoiceActionDispatch(dispatchAction, 'TOUCHBACK_CHOICE');
 
-    return (
-        <div>
-            <ActionButton onClick={() => dispatch('TOUCHBACK')}>TOUCHBACK</ActionButton>
-            <ActionButton onClick={() => dispatch('RETURN')}>ROLL</ActionButton>
-        </div>
-    );
+    return <ActionButtonGroup
+        onClick={dispatch}
+        gameVersion={game.version}
+        buttons={[
+            {key: 'TOUCHBACK', children: 'Touchback'},
+            {key: 'RETURN', children: 'Roll!'}
+        ]}
+    />;
 }
 
-function RollAgainChoicePane({dispatchAction}: {dispatchAction: ActionDispatch}) {
+function RollAgainChoicePane({dispatchAction, game}: ActionPaneProps) {
     const dispatch = getChoiceActionDispatch(dispatchAction, 'ROLL_AGAIN_CHOICE');
 
-    return (
-        <div>
-            <ActionButton onClick={() => dispatch('ROLL')}>ROLL</ActionButton>
-            <ActionButton onClick={() => dispatch('HOLD')}>HOLD</ActionButton>
-        </div>
-    );
+    return <ActionButtonGroup
+        onClick={dispatch}
+        gameVersion={game.version}
+        buttons={[
+            {key: 'ROLL', children: 'Roll!'},
+            {key: 'HOLD', children: 'Hold'}
+        ]}
+    />;
 }
 
-function RollPane({dispatchAction, game}: {dispatchAction: ActionDispatch, game: Game}) {
+function RollPane({dispatchAction, game}: ActionPaneProps) {
 
-    function dispatchRollAction(count: number) {
+    function dispatchRollAction(count: string) {
         dispatchAction({
-            name: "ROLL",
+            name: 'ROLL',
             count: count
         });
     }
-
-    let count = 1;
-    switch (game.state) {
-        case 'KICKOFF':
-
-    }
     
-    const rollButtons = getDieCountChoices(game).map(count =>
-        <ActionButton key={count} onClick={() => dispatchRollAction(count)}>{count}</ActionButton>);
+    const rollButtons = getDieCountChoices(game).map(count => {return {
+        key: ''+count, children: `${count}`
+    }});
 
     return (
         <div>
             <span>Roll:</span>
-            {rollButtons}
+            <ActionButtonGroup
+                gameVersion={game.version}
+                onClick={dispatchRollAction}
+                buttons={rollButtons} />
         </div>
     );
 }
 
-function CallPlayPane({dispatchAction, game}: {dispatchAction: ActionDispatch, game: Game}) {
-    
-    function dispatchPlayAction(play: Play) {
+function CallPlayPane({dispatchAction, game}: ActionPaneProps) {
+
+    function onClick(play: Play) {
         dispatchAction({
-            name: "CALL_PLAY",
+            name: 'CALL_PLAY',
             play: play
         });
     }
-    
-    return (
-        <div>
-            <ActionButton onClick={() => dispatchPlayAction('SHORT_RUN')}>Short Run</ActionButton>
-            <ActionButton onClick={() => dispatchPlayAction('LONG_RUN')}>Long Run</ActionButton>
-        </div>
-    );
+
+    return <ActionButtonGroup
+        onClick={onClick}
+        gameVersion={game.version}
+        buttons={[
+            {key: 'SHORT_RUN', children: 'Short Run'},
+            {key: 'LONG_RUN', children: 'Long Run'}
+        ]}
+    />;
 }
 
-function PatPane({dispatchAction, game}: {dispatchAction: ActionDispatch, game: Game}) {
+function PatPane({dispatchAction, game}: ActionPaneProps) {
     const dispatch = getChoiceActionDispatch(dispatchAction, 'PAT_CHOICE');
 
-    return (
-        <div>
-            <ActionButton onClick={() => dispatch('ONE_POINT')}>One Point</ActionButton>
-            <ActionButton onClick={() => dispatch('TWO_POINT')}>Two Point</ActionButton>
-        </div>
-    )
+    return <ActionButtonGroup
+        onClick={dispatch}
+        gameVersion={game.version}
+        buttons={[
+            {key: 'ONE_POINT', children: 'One Point'},
+            {key: 'TWO_POINT', children: 'Two Point'}
+        ]}
+    />;
 }
 
 function getChoiceActionDispatch(dispatchAction: ActionDispatch, choiceName: string) {
@@ -165,9 +179,72 @@ function getChoiceActionDispatch(dispatchAction: ActionDispatch, choiceName: str
     }
 }
 
-function ActionButton(props: {onClick: () => void, children: React.ReactNode}) {
+type ActionButtonProps<K extends string> = {
+    // A unique key for this button.
+    key: K;
+    // The Components to place in the button
+    children: React.ReactNode;
+};
+
+/**
+ * Display a group of buttons. These buttons can only be pressed once per game version.
+ * If a new game version is given, the buttons re-validate.
+ */
+function ActionButtonGroup<K extends string>({buttons, onClick, gameVersion}:
+    {buttons: ActionButtonProps<K>[], onClick: (key: K) => void, gameVersion: number}) {
+
+    // TODO: in the case where RspChoice -> RspChoice, the buttons never re-enable
+    
+    // the key of the button which was pressed
+    const [pressedButton, setPressedButton] = useState<K | null>(null);
+    // the game version when the button was pressed
+    const [pressedVersion, setPressedVersion] = useState<number | null>(null);
+
+    if (pressedVersion !== null && pressedVersion !== gameVersion) {
+        setPressedButton(null);
+        setPressedVersion(null);
+    }
+
+    function getButtonState(key: K): ActionButtonState {
+        if (pressedButton === null) {
+            return 'ENABLED';
+        }
+        if (pressedButton === key) {
+            return 'PRESSED';
+        }
+        return 'DISABLED';
+    }
+
+    function onClickFacade(key: K) {
+        if (getButtonState(key) === 'ENABLED') {
+            onClick(key);
+            setPressedButton(key);
+            setPressedVersion(gameVersion);
+        }
+    }
+
+    const buttonComponents = buttons.map(buttonProps =>
+        <ActionButton key={buttonProps.key}
+            onClick={() => onClickFacade(buttonProps.key)}
+            state={getButtonState(buttonProps.key)}>
+            
+            {buttonProps.children}
+        
+        </ActionButton>);
+    
+    return <div className='actionButtonGroup'>
+        {buttonComponents}
+    </div>;
+}
+
+type ActionButtonState = 'ENABLED' | 'DISABLED' | 'PRESSED';
+
+function ActionButton(props: {state: ActionButtonState, onClick: () => void, children: React.ReactNode}) {
+    
+    const className = `actionButton ${props.state}`;
+    
     return (
-        <button className="actionButton" onClick={props.onClick}>
+        <button className={className} onClick={props.onClick} disabled={props.state !== 'ENABLED'}>
             {props.children}
         </button>
     );
